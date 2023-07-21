7266e3a2754e9df889dd1bbf0218ae2e2a0bcd27
stanley <stanley6453@gmail.com>
2023-07-22 00:31:02 +0100

    added comment to server init method in 0x00-pagination/1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -26,6 +26,7 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initialize the server"""
         self.__dataset = None
 
     def dataset(self) -> List[List]:

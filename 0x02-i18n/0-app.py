129ccb053067de35874dcc7e64c1f1b7b8d6227f
stanley <stanley6453@gmail.com>
2023-08-01 13:11:44 +0100

    created a basic flask route

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,30 +1,11 @@
 #!/usr/bin/env python3
 """i18n compliant flask app"""
-
 from flask import Flask, render_template
-from flask_babel import Babel
 
 app = Flask(__name__)
 
-# Configure available languages
-
-
-class Config:
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
-
-# Initialize Babel
-babel = Babel(app)
-
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
-    return render_template('1-index.html')
-
-
-if __name__ == '__main__':
-    app.run()
+def index():
+    """main route"""
+    return render_template('0-index.html')

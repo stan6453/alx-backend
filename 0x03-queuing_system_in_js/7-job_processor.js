d9fbb5d3cdf18f12f11deb469974253861e75a43
stanley <stanley6453@gmail.com>
2023-08-29 12:09:09 +0100

    Implemented a queuing system using Redis pub/sub and Kue

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -2,7 +2,7 @@ import kue from 'kue';
 
 const queue = kue.createQueue();
 
-const blacklistedPhoneNumbers = [4153518780, 4153518781];
+const blacklistedPhoneNumbers = ['4153518780', '4153518781'];
 
 queue.process('push_notification_code_2', 2, function (job, done) {
   sendNotification(job.data.phoneNumber, job.data.message, job, done);
@@ -11,8 +11,8 @@ queue.process('push_notification_code_2', 2, function (job, done) {
 function sendNotification(phoneNumber, message, job, done) {
   job.progress(0, 100);
 
-  if (blacklistedPhoneNumbers.includes(phoneNumber)) { 
-    job.fail(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  if (blacklistedPhoneNumbers.includes(phoneNumber)) {
+    job.failed().error(new Error(`Phone number ${phoneNumber} is blacklisted`));
     return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
   }
 
